47bce1fd0cf216f214f9a743bd3f22cf7da1d992
Nhu Trang <trangnguyen14012005@gmail.com>
2025-10-31 21:37:58 +0700

    Update project structure and code

---
+++ app/src/main/java/com/example/demo/models/CartItem.java
@@ -0,0 +1,9 @@
+package com.example.demo.models;
+
+public class CartItem {
+    private String id;
+    private String name;
+    private String image;
+    private int price;
+    private int quantity;
+}
---
+++ app/src/main/java/com/example/demo/ui/cart/CartActivity.java
@@ -0,0 +1,4 @@
+package com.example.demo.ui.cart;
+
+public class CartActivity {
+}
